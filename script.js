7899175148c50b1aae86b891e43a4583c98fbebd
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-07-09 14:25:21 -0500

    added aside and main section

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
 // Add any styling you like!
 // Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API. Use DevTools to test for low-end devices.
 // Push that baby to github and share your solution below!
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
