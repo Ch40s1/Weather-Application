196da4620dfe6aa157a165fb49ad8451d247513b
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-07-10 14:49:13 -0500

    linked jquery

--- script.js
+++ script.js
@@ -15,3 +15,5 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+// select everything that we are going to work with
